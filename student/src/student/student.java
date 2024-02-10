009dc987d699cfd72a01fe5f66aaf382823a0aa2
Maynardshangomes <maynardgomes27@gmail.com>
2024-02-10 05:34:34 +0530

    message

---
+++ student/src/student/student.java
@@ -0,0 +1,15 @@
+package student;
+
+public class student {
+	
+	String name,rollno;
+	int age;
+	
+	student(String name,String rollno,int age)
+	{
+		this.name=name;
+		this.rollno=rollno;
+		this.age=age;
+	}
+
+}
